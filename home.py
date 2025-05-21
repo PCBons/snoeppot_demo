38347d09cecc490097ebf25b9d6b187b1bad9aa9
PCBons <43748923+PCBons@users.noreply.github.com>
2025-05-21 15:10:00 +0200

    Add streamlit script

---
+++ home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+if 'lijst' not in st.session_state:
+	st.session_state['lijst'] = []
+
+
+gok = st.number_input('Gok het aantal snoepjes')
+st.session_state['lijst'].append(gok)
+
+st.write(st.session_state['lijst'])
+st.write(len(st.session_state['lijst']))
+
+wis = st.button('wis de data')
+if wis:
+	st.session_state['lijst'] = []
+	st.rerun()
